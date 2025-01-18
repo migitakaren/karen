4f76cbd8a7823fb3812ed2049627e91c89efcfc7
migitakaren <168495203+migitakaren@users.noreply.github.com>
2025-01-18 22:29:26 +0900

    Create ex1-1.js

---
+++ ex1-1.js
@@ -0,0 +1,7 @@
+function imgchange1() {
+    document.getElementById('monstar').src = './image/2.jpg';
+}
+function imgchange2() {
+    document.getElementById('monstar').src = './image/1.jpg';
+}
+
