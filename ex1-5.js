1d5f73bc45bd49609c53090740b357b40e8725fe
migitakaren <168495203+migitakaren@users.noreply.github.com>
2025-01-18 22:58:25 +0900

    Update ex1-5.js

--- ex1-5.js
+++ ex1-5.js
@@ -1,2 +1,2 @@
-function nazo3(){if(document.getElementById('answer3').value!='ブー'){alert('はずれ!');}
+function nazo3(){if(document.getElementById('answer3').value!='エルサ'){alert('はずれ!');}
                 else{alert('あたり！');}}
