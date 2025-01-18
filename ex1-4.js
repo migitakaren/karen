84f70e6640ddb4aa81a3b011382377c55900112f
migitakaren <168495203+migitakaren@users.noreply.github.com>
2025-01-18 22:59:11 +0900

    Update ex1-4.js

--- ex1-4.js
+++ ex1-4.js
@@ -1,2 +1,2 @@
-function nazo2(){if(document.getElementById('answer2').value=='マイク・ワゾウスキ'){alert('あたり!');}
+function nazo2(){if(document.getElementById('answer2').value=='オラフ'){alert('あたり!');}
                 else{alert('はずれ');}}
